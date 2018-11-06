36f900305f04c854d45451107619220bb180f83c
HighClassGrey <tamekaahannah@gmail.com>
2018-11-05 18:05:15 -0600

    finished

---
+++ hoisting.js
@@ -0,0 +1,41 @@
+// name = 'Tameka'
+// var name;
+// console.log(name);
+
+setName();
+function setName() {
+    var name = 'Tameka';
+    console.log(name);
+}
+
+console.log('some text1');
+let avg = findAvg(2, 2);
+console.log('some text2', avg);
+function findAvg(a, b) {
+    console.log('some text3');
+    var answer = (a + b) / 2;
+    return answer;
+}
+let fruits = ['apple', 'tomato', 'banana'];
+let favFruit;
+function printFruits() {
+    favFruit = fruits[2];
+    console.log(fruits[0]);
+}
+
+function printFavFruit() {
+    console.log(favFruit);
+}
+
+printFruits();
+printFavFruit();
+
+someFunc();
+function someFunc() {
+
+}
+nameFunc();
+function nameFunc() {
+    alert('This is my Name:')
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
